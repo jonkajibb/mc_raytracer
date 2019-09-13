afbda8731a746bdb8cc2ec2cab74fefbfc28c44a
Unknown <jonazz.97@gmail.com>
2019-09-13 12:20:42 +0200

    mer headers

---
+++ Raytracer/Raytracer/Vertex.cpp
@@ -0,0 +1,9 @@
+#include "Vertex.h"
+
+Vertex::Vertex(float x, float y, float z) {
+
+}
+
+Vertex::~Vertex() {
+
+}
---
+++ Raytracer/Raytracer/Vertex.h
@@ -0,0 +1,14 @@
+
+class Vertex {
+
+public:
+	//Contructor
+	Vertex(float x, float y, float z);
+
+	//Destructor
+	~Vertex();
+
+
+private:
+
+}
