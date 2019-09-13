5a190eb9728f8b2599a48f8de128121737b18682
Unknown <jonazz.97@gmail.com>
2019-09-13 12:02:20 +0200

    1:a commiten / Skapade VS-projekt
    Gjort klasserna Scene och Triangle.

---
+++ Raytracer/Raytracer/Scene.h
@@ -0,0 +1,7 @@
+//class scene. contains instances of class triangle.
+//scene objects are closed (?).
+class Scene
+{
+public:
+private:
+};
---
+++ Raytracer/Raytracer/Triangle.h
@@ -0,0 +1,18 @@
+//triangle class. Defined by three objects from class Vertex.
+//Has color represented by instance of ColorDbl.
+//Has normal stored as an instance of Direction.
+class Triangle
+{
+public:
+	//Constructor
+	Triangle();
+
+	//Destructor
+	~Triangle();
+
+	void createTriangle();
+
+	void clean();
+
+private:
+};
